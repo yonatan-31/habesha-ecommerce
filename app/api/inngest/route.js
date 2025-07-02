202635e76d5da19eafa8e3a09b060cc5729ede61
yonatan <yonatanasmare31@gmail.com>
2025-07-02 05:40:35 +0300

    change 2

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -8,6 +8,6 @@ export const { GET, POST, PUT } = serve({
         syncUserCreation,
         syncUserUpdate,
         syncUserDeletion,
-        createUserOrder
+        syncUserCreation
     ],
 });
