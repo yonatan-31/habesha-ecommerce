f16f11ba74220a26400b38f8f0b834fd842cccd2
yonatan <yonatanasmare31@gmail.com>
2025-07-02 10:01:40 +0300

    final

--- config/inngest.js
+++ config/inngest.js
@@ -2,8 +2,7 @@
 
 import { Inngest } from "inngest";
 
-export const inngest = new Inngest();
-// export const inngest = new Inngest({
-//   id: "quickcart-next", 
-// });
+export const inngest = new Inngest({
+  id: "quickcart-next", 
+});
 
